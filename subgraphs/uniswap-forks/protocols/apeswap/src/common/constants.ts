a47e9ea3c04063ffbead1df503f35723377cd0e7
Chris Steege <chris@Chriss-MacBook-Pro.local>
2022-05-24 21:47:42 -0400

    test

--- subgraphs/uniswap-forks/protocols/apeswap/src/common/constants.ts
+++ subgraphs/uniswap-forks/protocols/apeswap/src/common/constants.ts
@@ -9,4 +9,4 @@ export const PROTOCOL_METHODOLOGY_VERSION = "1.0.0";
 export const PROTOCOL_NAME = "Apeswap";
 export const PROTOCOL_SLUG = "apeswap";
 
-log.warning("Hello27", [])
+log.warning("Hello28", [])
