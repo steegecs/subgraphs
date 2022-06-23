e46afc12ef96f223633b2bfbbf0aba67c8e9dc77
Chris Steege <steegecas@gmail.com>
2022-06-22 21:17:39 -0400

    Test

--- subgraphs/uniswap-forks/protocols/quickswap/src/common/constants.ts
+++ subgraphs/uniswap-forks/protocols/quickswap/src/common/constants.ts
@@ -3,6 +3,6 @@
 ////////////////////
 
 export const PROTOCOL_SUBGRAPH_VERSION = "1.0.0";
-export const PROTOCOL_METHODOLOGY_VERSION = "1.0.0";
+export const PROTOCOL_METHODOLOGY_VERSION = "1.0.0"
 export const PROTOCOL_NAME = "QuickSwap"
 export const PROTOCOL_SLUG = "quickswap"
