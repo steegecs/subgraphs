d54aebaeb0017a948716bcca95c2d95548d52b42
Chris Steege <steegecas@gmail.com>
2022-12-09 10:35:26 -0600

    Testing uniswap v2

--- subgraphs/uniswap-forks/src/mappings/factory.ts
+++ subgraphs/uniswap-forks/src/mappings/factory.ts
@@ -1,7 +1,5 @@
 import { log } from "@graphprotocol/graph-ts";
-
-import { PairCreated } from "../../generated/Factory/Factory";
-import { createLiquidityPool } from "../common/creators";
+import { PairCreated, SetFeeToCall } from "../../generated/Factory/Factory";
 
 // Handle the creation of a new liquidity pool from the Factory contract
 // Create the pool entity and track events from the new pool contract using the template specified in the subgraph.yaml
@@ -11,40 +9,8 @@ export function handlePairCreated(event: PairCreated): void {
     event.params.token0.toHexString(),
     event.params.token1.toHexString(),
   ]);
-  createLiquidityPool(
-    event,
-    event.params.pair.toHexString(),
-    event.params.token0.toHexString(),
-    event.params.token1.toHexString()
-  );
 }
 
-// The call handler is used to update feeTo as on or off for each pool
-// export function handleFeeTo(call: SetFeeToCall): void {
-//   let protocol = getOrCreateDex()
-//   let poolIds = protocol._poolIds
-//   let lpFeeUpdate: BigDecimal
-//   let protocolFeeUpdate: BigDecimal
-//   if (call.inputs._feeTo.toHexString() != ZERO_ADDRESS)  {
-//     lpFeeUpdate = LP_FEE_TO_ON
-//     protocolFeeUpdate = PROTOCOL_FEE_TO_ON
-//   } else {
-//     lpFeeUpdate = LP_FEE_TO_OFF
-//     protocolFeeUpdate = PROTOCOL_FEE_TO_OFF
-//   }
-//     for (let i = 0; i < poolIds.length; i++) {
-//       // will need blockNumber param
-//       let pool = getLiquidityPool(poolIds[i].toHexString())
-//       let lpFeeId = pool.fees[0]
-//       let protocolFeeId = pool.fees[1]
-
-//       let lpFee = getLiquidityPoolFee(lpFeeId)
-//       lpFee.feePercentage = lpFeeUpdate
-
-//       let protocolFee = getLiquidityPoolFee(protocolFeeId)
-//       protocolFee.feePercentage = protocolFeeUpdate
-
-//       lpFee.save()
-//       protocolFee.save()
-//   }
-// }
+export function handleFeeTo(call: SetFeeToCall): void {
+  log.warning("FeeTo: {}", [call.inputs._feeTo.toHexString()]);
+}
--- subgraphs/uniswap-forks/src/mappings/pool.ts
+++ subgraphs/uniswap-forks/src/mappings/pool.ts
@@ -5,119 +5,33 @@ import {
   Transfer,
   Sync,
 } from "../../generated/templates/Pair/Pair";
-import {
-  createDeposit,
-  createWithdraw,
-  createSwapHandleVolumeAndFees,
-} from "../common/creators";
-import {
-  handleTransferBurn,
-  handleTransferMint,
-  handleTransferToPoolBurn,
-} from "../common/handlers";
-import {
-  updateFinancials,
-  updateInputTokenBalances,
-  updatePoolMetrics,
-  updateTvlAndTokenPrices,
-  updateUsageMetrics,
-} from "../common/updateMetrics";
-import {
-  BIGINT_THOUSAND,
-  BIGINT_ZERO,
-  UsageType,
-  ZERO_ADDRESS,
-} from "../common/constants";
-import { getLiquidityPool } from "../common/getters";
+import { log } from "@graphprotocol/graph-ts";
 
 // Handle transfers event.
 // The transfers are either occur as a part of the Mint or Burn event process.
 // The tokens being transferred in these events are the LP tokens from the liquidity pool that emitted this event.
 export function handleTransfer(event: Transfer): void {
-  const pool = getLiquidityPool(
-    event.address.toHexString(),
-    event.block.number
-  );
-
-  // ignore initial transfers for first adds
-  if (
-    event.params.to.toHexString() == ZERO_ADDRESS &&
-    event.params.value.equals(BIGINT_THOUSAND) &&
-    pool.outputTokenSupply == BIGINT_ZERO
-  ) {
-    return;
-  }
-  // mints
-  if (event.params.from.toHexString() == ZERO_ADDRESS) {
-    handleTransferMint(
-      event,
-      pool,
-      event.params.value,
-      event.params.to.toHexString()
-    );
-  }
-  // Case where direct send first on native token withdrawls.
-  // For burns, mint tokens are first transferred to the pool before transferred for burn.
-  // This gets the EOA that made the burn loaded into the _Transfer.
-
-  if (event.params.to == event.address) {
-    handleTransferToPoolBurn(event, event.params.from.toHexString());
-  }
-  // burn
-  if (
-    event.params.to.toHexString() == ZERO_ADDRESS &&
-    event.params.from == event.address
-  ) {
-    handleTransferBurn(
-      event,
-      pool,
-      event.params.value,
-      event.params.from.toHexString()
-    );
-  }
+  log.warning("Transfer event: {}", [event.params.to.toHexString()]);
 }
 
 // Handle Sync event.
 // Emitted after every Swap, Mint, and Burn event.
 // Gives information about the rebalancing of tokens used to update tvl, balances, and token pricing
 export function handleSync(event: Sync): void {
-  updateInputTokenBalances(
-    event.address.toHexString(),
-    event.params.reserve0,
-    event.params.reserve1,
-    event.block.number
-  );
-  updateTvlAndTokenPrices(event.address.toHexString(), event.block.number);
+  log.warning("Sync event: {}", [event.params.reserve0.toString()]);
 }
 
 // Handle a mint event emitted from a pool contract. Considered a deposit into the given liquidity pool.
 export function handleMint(event: Mint): void {
-  createDeposit(event, event.params.amount0, event.params.amount1);
-  updateUsageMetrics(event, event.params.sender, UsageType.DEPOSIT);
-  updateFinancials(event);
-  updatePoolMetrics(event);
+  log.warning("Mint event: {}", [event.address.toHexString()]);
 }
 
 // Handle a burn event emitted from a pool contract. Considered a withdraw into the given liquidity pool.
 export function handleBurn(event: Burn): void {
-  createWithdraw(event, event.params.amount0, event.params.amount1);
-  updateUsageMetrics(event, event.transaction.from, UsageType.WITHDRAW);
-  updateFinancials(event);
-  updatePoolMetrics(event);
+  log.warning("Burn event: {}", [event.address.toHexString()]);
 }
 
 // Handle a swap event emitted from a pool contract.
 export function handleSwap(event: Swap): void {
-  createSwapHandleVolumeAndFees(
-    event,
-    event.params.to.toHexString(),
-    event.params.sender.toHexString(),
-    event.params.amount0In,
-    event.params.amount1In,
-    event.params.amount0Out,
-    event.params.amount1Out
-  );
-  updateFinancials(event);
-  updatePoolMetrics(event);
-  updateUsageMetrics(event, event.transaction.from, UsageType.SWAP);
+  log.warning("Swap event: {}", [event.address.toHexString()]);
 }
